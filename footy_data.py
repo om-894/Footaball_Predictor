8abaea7e5ff67602733b7ab4d4a05370e150d731
om-894 <158072423+om-894@users.noreply.github.com>
2024-10-24 17:41:26 +0100

    updated data folder

--- footy_data.py
+++ footy_data.py
@@ -9,7 +9,7 @@ import re
 import urllib3
 
 # Create the "data" folder if it doesn't exist
-data_folder = "data_raw"
+data_folder = "data/data_raw"
 if not os.path.exists(data_folder):
     os.makedirs(data_folder)
 
@@ -342,6 +342,39 @@ print(md10_df) # to display the dataframe with the new column names
 
 # Export to CSV
 md10_df.to_csv(os.path.join(data_folder, "sunderland_md10_player_stats.csv"), index=False)
+
+
+####################################################
+#         Match 11: Sunderland vs Luton town       #
+####################################################
+
+md11_url = "https://fbref.com/en/matches/b2475a0d/Luton-Town-Sunderland-October-23-2024-Championship"
+response = requests.get(md11_url, verify=False)
+md11_df = pd.read_html(response.text, attrs={"id":"stats_8ef52968_summary"})[0]
+
+# Join multiindex columns with an underscore and remove extra spaces
+md11_df.columns = ['_'.join(col).strip() for col in md11_df.columns.values]
+
+# Clean column names using regex
+md11_df.columns = [re.sub(r'Unnamed:.*?_level_0_', '', col).strip() for col in md11_df.columns]
+
+# remove bottom row so that we only have individual player data.
+md11_df = md11_df.iloc[:-1]
+
+# Assuming your dataframe is named 'df'
+md11_df['Saves'] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3]
+md11_df['Fouls_Committed'] = [1, 0, 3, 0, 2, 0, 0, 0, 3, 2, 0, 0, 1, 0]
+md11_df['Fouls_Won'] = [0, 1, 1, 0, 1, 0, 3, 0, 3, 2, 1, 0, 1, 1]
+
+print(md11_df) # to display the dataframe with the new column names
+
+# Export to CSV
+md11_df.to_csv(os.path.join(data_folder, "sunderland_md11_player_stats.csv"), index=False)
+
+
+####################################################
+
+
 
 
 
